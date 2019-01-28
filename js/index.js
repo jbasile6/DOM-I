7e6f692e85960bccde82931261b7664d4d400a5f
James Basile <jbasile14@gmail.com>
2019-01-28 18:32:58 -0500

    completed task 3

--- js/index.js
+++ js/index.js
@@ -46,7 +46,6 @@ const navBar = document.querySelectorAll('a');
 //Attempted to use forEach to run through each 'a' tag and give it 'nav-item-x' content
 //Will come back and try for loop once MVP complete, writing them out one by one for now
 
-
 // navBar.forEach(navItem => navItem.textContent = siteContent['nav'][`nav-item-${navBar.length -= (navBar.length -= 1)}`]);
 
 
@@ -59,10 +58,30 @@ navBar[5].textContent = siteContent['nav']['nav-item-6'];
 
 
 //Section CTA-----------------------------------------------
+//h1 needs line breaks
+//const lineBreak = document.createElement('br');
+
 
 //h1
 const domIsAwesome = document.querySelector('h1');
-domIsAwesome.textContent = siteContent['cta']['h1'];
+const beforeH1 = document.createElement('h1');
+const afterH1 = document.createElement('h1');
+const newH1Array = siteContent['cta']['h1'].split(' ');
+
+
+const allCtaH1 = document.querySelectorAll('.cta-text h1');
+//console.log(allCtaH1);
+
+
+//console.log(h1Array);
+
+beforeH1.textContent = newH1Array[0];
+domIsAwesome.textContent = newH1Array[1];
+afterH1.textContent = newH1Array[2];
+
+//***note to self:APPENDS/PREPENDS MUST BE AT THE BOTTOM!!!!
+domIsAwesome.append(afterH1);
+domIsAwesome.prepend(beforeH1);
 
 //button
 const button = document.querySelector('button');
@@ -77,3 +96,52 @@ circleImg.alt = "Header Cirlce Image";
 
 
 //MAIN CONTENT------------------------------------------------
+//array to carry each h4 location
+const mainContHFours = document.querySelectorAll('.main-content .text-content h4');
+//array to carry each p location
+const mainContentPs = document.querySelectorAll('.main-content .text-content p');
+
+//console.log(mainContHFours);
+//console.log(mainContentPs);
+
+//features
+mainContHFours[0].textContent = siteContent['main-content']['features-h4'];
+mainContentPs[0].textContent = siteContent['main-content']['features-content'];
+
+//about
+mainContHFours[1].textContent = siteContent['main-content']['about-h4'];
+mainContentPs[1].textContent = siteContent['main-content']['about-content'];
+
+//services
+mainContHFours[2].textContent = siteContent['main-content']['services-h4'];
+mainContentPs[2].textContent = siteContent['main-content']['services-content'];
+
+//product
+mainContHFours[3].textContent = siteContent['main-content']['product-h4'];
+mainContentPs[3].textContent = siteContent['main-content']['product-content'];
+
+//vision
+mainContHFours[4].textContent = siteContent['main-content']['vision-h4'];
+mainContentPs[4].textContent = siteContent['main-content']['vision-content'];
+
+//middle-img
+const middleImg = document.getElementById('middle-img');
+middleImg.src = siteContent['main-content']["middle-img-src"];
+middleImg.alt = 'Middle Content Image';
+//----------------------------------------------------------------
+
+//Contact-----------------------------------------------------------
+const contactHFour = document.querySelectorAll('.contact h4');
+const contactPs = document.querySelectorAll('.contact p');
+
+contactHFour.textContent = siteContent['contact']['contact-h4'];
+contactPs[0].textContent = siteContent['contact']['address'];
+contactPs[1].textContent = siteContent['contact']['phone'];
+contactPs[2].textContent = siteContent['contact']['email'];
+
+//-------------------------------------------------------------------
+
+//Footer------------------------------------------------------
+const footerP = document.querySelector('footer p');
+
+footerP.textContent = siteContent['footer']['copyright'];
